9535f7cd7843f504a6c75113e9049ace03f81962
Rosita <Rosita@dbotia>
2015-07-25 10:50:47 -0500

    Commit Modificado

---
+++ build/generated-sources/ap-source-output/entities/DiscountCode_.java
@@ -0,0 +1,18 @@
+package entities;
+
+import entities.Customer;
+import java.math.BigDecimal;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-07-25T10:43:42")
+@StaticMetamodel(DiscountCode.class)
+public class DiscountCode_ { 
+
+    public static volatile SingularAttribute<DiscountCode, BigDecimal> rate;
+    public static volatile CollectionAttribute<DiscountCode, Customer> customerCollection;
+    public static volatile SingularAttribute<DiscountCode, String> discountCode;
+
+}
